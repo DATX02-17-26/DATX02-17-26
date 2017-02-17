e1e7c756f2b0903bd1d1d653d68734fdf929059e
Mazdak Farrokhzad <twingoow@gmail.com>
2017-02-17 17:29:41 +0100

    add temp sanity test for parser

--- Test/TastyTests.hs
+++ Test/TastyTests.hs
@@ -3,10 +3,16 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
+import Language.Java.Parser as JP
+import Language.Java.Syntax as JA
+
 -- | HUnit unit tests
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "1 == 1" $ 1 @?= (1 :: Int)
+  , testCase "parser" $
+        JP.parser JP.exp "1 + 2 * 3" @?=
+        Right (BinOp (Lit (Int 1)) Add (BinOp (Lit (Int 2)) Mult (Lit (Int 3))))
   ]
 
 -- | QuickCheck property based tests
