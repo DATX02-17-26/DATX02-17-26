646c79efa01dab0989ebefa0c9966758c1b2897d
Mazdak Farrokhzad <twingoow@gmail.com>
2017-02-18 16:50:56 +0100

    test jenkins integration

--- Test/TastyTests.hs
+++ Test/TastyTests.hs
@@ -11,8 +11,8 @@ unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "1 == 1" $ 1 @?= (1 :: Int)
   , testCase "parser" $
-        JP.parser JP.exp "1 + 2 * 3" @?=
-        Right (BinOp (Lit (Int 1)) Add (BinOp (Lit (Int 2)) Mult (Lit (Int 3))))
+        JP.parser JP.exp "2 + 2 * 3" @?=
+        Right (BinOp (Lit (Int 2)) Add (BinOp (Lit (Int 2)) Mult (Lit (Int 3))))
   ]
 
 -- | QuickCheck property based tests
