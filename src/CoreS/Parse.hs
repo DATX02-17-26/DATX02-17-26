e9ea35a25bcb8bbe2a04ff890cd65e415e83ec38
Mazdak Farrokhzad <twingoow@gmail.com>
2017-02-22 20:47:56 +0100

    CoreS.Parse: add missing license header

--- src/CoreS/Parse.hs
+++ src/CoreS/Parse.hs
@@ -1,3 +1,21 @@
+{- DATX02-17-26, automated assessment of imperative programs.
+ - Copyright, 2017, see AUTHORS.md.
+ -
+ - This program is free software; you can redistribute it and/or
+ - modify it under the terms of the GNU General Public License
+ - as published by the Free Software Foundation; either version 2
+ - of the License, or (at your option) any later version.
+ -
+ - This program is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU General Public License for more details.
+ -
+ - You should have received a copy of the GNU General Public License
+ - along with this program; if not, write to the Free Software
+ - Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ -}
+
 module CoreS.Parse where
 
 import Data.Bifunctor (first)
