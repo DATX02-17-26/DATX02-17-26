05b17e78f942f700f51f673e8d2eea86f4fd7cee
Maximilian Algehed <m.algehed@gmail.com>
2017-02-25 16:07:03 +0100

    fixed

--- libsrc/PropertyBasedTesting.hs
+++ libsrc/PropertyBasedTesting.hs
@@ -62,7 +62,7 @@ testSolutions :: FilePath -> String -> EvalM Bool
 testSolutions dir input = do
   modelOutputs <- modelSolutionsOutputs dir input
   studO <- studentOutput dir input
-  return $ maybe False (\s -> or [s == output | output <- modelOutputs]) stud0
+  return $ maybe False (\s -> or [s == output | output <- modelOutputs]) studO
 
 -- | Perform the relevant tests on all class files in the directory
 -- `dir`, returns `True` if the student solution passes all tests
