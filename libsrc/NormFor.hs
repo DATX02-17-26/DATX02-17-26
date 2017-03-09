494ffdb7503db6b9467406ea5ba4abca0baf0f82
alekaar <sternkaar@gmail.com>
2017-03-09 18:20:12 +0100

    restructured code, more readable

--- libsrc/NormFor.hs
+++ libsrc/NormFor.hs
@@ -3,11 +3,21 @@ module NormFor where
 import CoreS.AST
 import Data.Maybe
 
+--The name of the normalization step
+name :: String
+name = "NormForToWhile"
+
+--The normalization stage
+stages :: [Int]
+stages = [1]
+
 --start at compunit, go inte each classbody
 execute :: CompilationUnit -> Maybe CompilationUnit
 execute cu = case cu of
-  CompilationUnit tds -> Just $ CompilationUnit $ intoClassTypeDecl <$> tds
-  hole -> Just hole
+  CompilationUnit tds ->
+    let normCU = CompilationUnit $ intoClassTypeDecl <$> tds
+    in if cu == normCU then Just normCU else Nothing
+  hole -> Nothing
 
 --start at classBody, go into ClassDecl
 intoClassTypeDecl :: TypeDecl -> TypeDecl
@@ -49,33 +59,48 @@ intoBlock block = case block of
   --MemberDecl $ MethodDecl mt ident fp $ Block $ intoStmt <$> stmts
 
 {-
-  looks like hell, fix with record wildcards.
   At the moment only changes for to while, adds a scope
   around it. Should work with holes
 -}
 intoStmt :: Stmt -> Stmt
 intoStmt stm = case stm of
-  SBlock (Block stmts)             -> SBlock $ Block $ intoStmt <$> stmts
-  SIf expr stmt                    -> (SIf expr (intoStmt stmt))
-  SIfElse expr stmt1 stmt2         -> SIfElse expr (intoStmt stmt1) (intoStmt stmt2)
-  SWhile expr stmt                 -> SWhile expr $ intoStmt stmt
-  SDo expr stmt                    -> SDo expr (intoStmt stmt)
-  SForB mForInit mExpr mExprs stmt ->
-    SBlock $ Block [(maybe SEmpty forInitToStmt mForInit),
-                    SWhile (fromMaybe (ELit (Boolean True)) mExpr)
-                    (addExpsIntoStmt mExprs (intoStmt stmt))]
-  SForE vmType ident expr stmt     -> SForE vmType ident expr (intoStmt stmt)
-  stmt                             -> stmt
+  SBlock (Block stmts)         -> SBlock $ Block $ intoStmt <$> stmts
+  SIf expr stmt                -> (SIf expr (intoStmt stmt))
+  SIfElse expr stmt1 stmt2     -> SIfElse expr (intoStmt stmt1) (intoStmt stmt2)
+  SWhile expr stmt             -> SWhile expr $ intoStmt stmt
+  SDo expr stmt                -> SDo expr (intoStmt stmt)
+  SForB mfi me mes s           -> addBlock $ forToWhile (SForB mfi me mes s)
+  SForE vmType ident expr stmt -> SForE vmType ident expr (intoStmt stmt)
+  stmt                         -> stmt
+
+
+--creates a block around a stmt
+addBlock :: [Stmt] -> Stmt
+addBlock stmt = SBlock $ Block stmt
+
+--takes a for-loop, converts it into a while by adding a block around it
+forToWhile :: Stmt -> [Stmt]
+forToWhile (SForB mForInit mExpr mExprs stmt) =
+  [(maybe SEmpty forInitToStmt mForInit),
+  SWhile (convForCond mExpr) (addExpsIntoStmt mExprs (intoStmt stmt))]
+
+
+--converts a maybe condition into a condition for a while-loop
+convForCond :: Maybe Expr -> Expr
+convForCond mExpr = fromMaybe (ELit (Boolean True)) mExpr
 
 {-
-  makes maybe exprs into statements and puts them
-  in same scope as the block as the stmts in a loop.
+  makes maybe exprs into SExprs and puts them
+  in same sblock if there is one, otherwise if only 1 statement
+  and no block, create a block around and add SExprs.
   Should work with holes.
 -}
 addExpsIntoStmt :: Maybe [Expr] -> Stmt -> Stmt
 addExpsIntoStmt mExprs stmt = case mExprs of
   Nothing -> stmt
-  (Just exprs) -> SBlock $ Block ([stmt] ++ (SExpr <$> exprs))
+  (Just exprs) -> case stmt of
+    SBlock (Block stmts) -> SBlock $ Block $ stmts ++ (SExpr <$> exprs)
+    _                    -> SBlock $ Block $ [stmt] ++ (SExpr <$> exprs)
 
 {-
   Depending on what kind of init, turn into stmts
