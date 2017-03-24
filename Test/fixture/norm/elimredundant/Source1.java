b2978b5598cdaf34a64fbccf7eef5f98017f6805
alekaar <sternkaar@gmail.com>
2017-03-24 13:57:47 +0100

    added test elim redundant

---
+++ Test/fixture/norm/elimredundant/Source1.java
@@ -0,0 +1,107 @@
+public class C{
+  public static void main(String[] args) {
+    {}
+
+    {
+      int i;
+    }
+
+    {;;;}
+
+    {int j;
+      {
+        ;;;;
+        {}
+      }
+    }
+    for(int o = 0; o < 3; o++){
+      int k;
+      {}
+
+      {
+        int g;
+      }
+
+      {;;;}
+
+      {int f;
+        {
+          ;;;;
+          {}
+        }
+      }
+    }
+    if(true){
+      int k;
+      {}
+
+      {
+        int g;
+      }
+
+      {;;;}
+
+      {int f;
+        {
+          ;;;;
+          {}
+        }
+      }
+    }
+    else{
+      int k;
+      {}
+
+      {
+        int g;
+      }
+
+      {;;;}
+
+      {int f;
+        {
+          ;;;;
+          {}
+        }
+      }
+    }
+    do{
+      int k;
+      {}
+
+      {
+        int g;
+      }
+
+      {;;;}
+
+      {int f;
+        {
+          ;;;;
+          {}
+        }
+      }
+    }while(true)
+    while(true){
+      int k;
+      {}
+
+      {
+        int g;
+      }
+      {;;;}
+      {int f;
+        {
+          ;;;;
+          {}
+        }
+      }
+    }
+
+    for (; ;) {}
+    if(true){}else{}
+    while(true){}
+    do{}while(true)
+    if(true){}
+  }
+}
