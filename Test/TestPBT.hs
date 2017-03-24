93064ac254c3eb54be21425c2b9a331342e2d468
Oboij <simon@boij.net>
2017-03-24 15:54:16 +0100

    adding my tests

--- Test/TestPBT.hs
+++ Test/TestPBT.hs
@@ -47,9 +47,23 @@ test2 = not <$> test
   (Ctx "Test/Student_solutions/sumNumbers1.java" ["Test/Model_solutions/sumNumbers0.java"])
   (makeGenerator (sumNumbers @NewlineString))
 
+test3 :: IO Bool
+test3 = test
+  (Ctx "Test/PbtTest/student/PbtStud2OK.java" ["Test/PbtTest/model/PbtMod1.java"
+  , "Test/PbtTest/model/PbtMod2.java"])
+  (makeGenerator (sumNumbers @NewlineString))
+
+test4 :: IO Bool
+test4 =  test
+  (Ctx "Test/PbtTest/student/PbtStud1Failjava" ["Test/PbtTest/model/PbtMod1.java"
+  , "Test/PbtTest/model/PbtMod2.java"])
+  (makeGenerator (sumNumbers @NewlineString))
+
 allTests :: TestTree 
 allTests = testGroup "PBT tests"
   [ testCase "helloWorld" $ assert test0
   , testCase "numbers-0"  $ assert test1
   , testCase "numbers-1"  $ assert test2
+  , testCase "printFromArgs" $ assert test3
+  , testCase "printFromScanner" $ assert test4
   ]
