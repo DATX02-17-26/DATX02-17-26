b424f95044e1f76d5e7e0c4633a94dd5724a8674
alekaar <sternkaar@gmail.com>
2017-03-30 16:40:04 +0200

    added some model solutions for task 12a

---
+++ modelsolution/uppgift12a/Uppgift12a_3.java
@@ -0,0 +1,17 @@
+public class Uppgift12a_3{
+  public static void main(String[]args){
+
+    double pi = 0;
+    double quotient;
+    for (int i = 1; i <= 500; i++) {
+      quotient = (1.0 / ((i * 2) - 1));
+      if (i%2 > 0){
+        pi = pi + quotient;
+      }
+      else{
+        pi = pi - quotient;
+      }
+    }
+    System.out.println(pi*4);
+  }
+}
