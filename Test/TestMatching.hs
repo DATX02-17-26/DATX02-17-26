267feb0907681e6b44b6d26572aa92dcfab321b7
Brage Salhus Bunk <bragesb@gmail.com>
2017-03-31 14:25:46 +0200

    Test for percentage of matching solutions

---
+++ Test/TestMatching.hs
@@ -0,0 +1,55 @@
+module TestMatching where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+import System.Directory
+import System.FilePath
+
+import EvaluationMonad
+import GenStrat
+import SolutionContext
+import AlphaR
+import NormalizationStrategies
+import CoreS.AST
+import qualified CoreS.ASTUnitype as AST
+import CoreS.ASTUnitypeUtils
+import CoreS.Parse
+import Data.RoseTree
+import Data.List
+
+main = do
+  args <- getArgs
+  case args of
+    [stud, mods] -> do
+      studs   <- hasExtension <$> listDirectory stud
+      mods    <- hasExtension <$> listDirectory mods
+      results <- mapM (\stud -> checkMatches stud mods) studs
+      let trues = [ () | (Just True) <- results ]
+          all   = [ () | (Just _) <- results ]
+          percentage = (genericLength trues / genericLength all) :: Double
+      putStrLn "Total %: " ++ show percentage
+    _ -> putStrLn "Bad args"
+
+normalizations :: Normalizer CompilationUnit
+normalizations = [ alphaRenaming ]
+
+normalize :: CompilationUnit -> CompilationUnit
+normalize = executeNormalizer normalizations
+
+normalizeUAST :: AST.AST -> AST.AST
+normalizeUAST  = AST.inCore normalize
+
+checkMatches :: FilePath -> [FilePath] -> IO (Maybe Bool)
+checkMatches stud mods = do
+  let paths = Ctx stud mods
+  (Ctx (stud) mods) <- resultEvalM ((fmap parseConv) <$> readRawContents paths)
+  case stud of
+    Left _     -> return Nothing
+    Right stud ->
+      return $ or [ matches
+                    normalizeUAST
+                    (AST.toUnitype $ normalize stud)
+                    (AST.toUnitype (normalize mod))
+                  | (Right mod) <- mods ]
