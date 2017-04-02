3254e7fd2322b3b905741215b8d9f5f8fa52b5e5
alekaar <sternkaar@gmail.com>
2017-04-02 13:34:57 +0200

    tested and working

---
+++ Test/fixture/norm/dowtowhile/Source1.java
@@ -0,0 +1,43 @@
+public class C{
+
+  public static void main(String[] args) {
+
+
+        for(int i = 0; i < 3; i++){
+          do;
+          while(true);
+        }
+
+        if(true){
+          do;
+          while(true);
+        }
+        else{
+          do;
+          while(true);
+        }
+
+        if(true){
+          do;
+          while(true);
+        }
+
+        while(true){
+          do;
+          while(true);
+        }
+
+        do{
+          do;
+          while(true);
+        }while(true);
+
+        {
+          do;
+          while(true);
+        }
+
+    do;
+    while(true);
+  }
+}
