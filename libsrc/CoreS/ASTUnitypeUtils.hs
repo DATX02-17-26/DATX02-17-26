9c02770b5bd09feb7a5ccf3e13873032ac9266b6
BeardedRoar <joeljoel224@hotmail.com>
2017-04-02 14:07:59 +0200

    operators in correct order

--- libsrc/CoreS/ASTUnitypeUtils.hs
+++ libsrc/CoreS/ASTUnitypeUtils.hs
@@ -32,106 +32,110 @@ import Data.Set (fromList, union, intersection)
 dependsOn :: AST -> AST -> Bool
 dependsOn SEmpty _ = False
 dependsOn _ SEmpty = False
+dependsOn (EMApp _ _) _ = True
+dependsOn _ (EMApp _ _) = True
 dependsOn a1 a2 =
-  (not $ null $ (fromList $ changesIds a1) `union` ((fromList $ changesIds a2) `intersection` (fromList $ changesIds a2))) ||
+  (not $ null $ (fromList $ changesIds a1) `intersection` ((fromList $ changesIds a2) `union` (fromList $ changesIds a2))) ||
   (not $ null $ (fromList $ usesIds a1) `intersection` (fromList $ changesIds a2))
 
 usesIds :: AST -> [C.Ident]
 usesIds = \case
-  (LVName n)                -> C._nmIds n
-  (LVArray a as)            -> um $ a:as
-  (InitExpr a)              -> usesIds a
-  (InitArr  as)             -> um as
-  (ELit a)                  -> usesIds a
-  (EVar a)                  -> usesIds a
-  (ECast _ a )              -> usesIds a
-  (ECond a1 a2 a3)          -> um [a1,a2,a3]
-  (EAssign a1 a2)           -> um [a1,a2]
-  (EOAssign a1 _ a2)        -> um [a1,a2]
-  (ENum _ a1 a2)            -> um [a1,a2]
-  (ECmp _ a1 a2)            -> um [a1,a2]
-  (ELog _ a1 a2)            -> um [a1,a2]
-  (ENot a)                  -> usesIds a
-  (EStep _ a)               -> usesIds a
-  (EBCompl a)               -> usesIds a
-  (EPlus a)                 -> usesIds a
-  (EMinus a)                -> usesIds a
-  (EMApp n as)              -> (C._nmIds n) ++ (um as) -- should the identifier parts of the name be in here?
-  (EArrNew  _ as _)         -> um as
-  (EArrNewI _ _ as)         -> um as
-  (ESysOut a)               -> usesIds a
-  (Block as)                -> um as
-  (SExpr a)                 -> usesIds a
-  (SVars d)                 -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d) -- must think about list of maybe VarInits as well
-  (SReturn a)               -> usesIds a
-  (SIf a1 a2)               -> um [a1,a2]
-  (SIfElse a1 a2 a3)        -> um [a1,a2,a3]
-  (SWhile a1 a2)            -> um [a1,a2]
-  (SDo a1 a2)               -> um [a1,a2]
-  (SForB ma1 ma2 mas a)     -> um $ a : maybeToList ma1 ++
+  LVName n                -> C._nmIds n
+  LVArray a as            -> um $ a:as
+  InitExpr a              -> usesIds a
+  InitArr  as             -> um as
+  ELit a                  -> usesIds a
+  EVar a                  -> usesIds a
+  ECast _ a               -> usesIds a
+  ECond a1 a2 a3          -> um [a1,a2,a3]
+  EAssign a1 a2           -> um [a1,a2]
+  EOAssign a1 _ a2        -> um [a1,a2]
+  ENum _ a1 a2            -> um [a1,a2]
+  ECmp _ a1 a2            -> um [a1,a2]
+  ELog _ a1 a2            -> um [a1,a2]
+  ENot a                  -> usesIds a
+  EStep _ a               -> usesIds a
+  EBCompl a               -> usesIds a
+  EPlus a                 -> usesIds a
+  EMinus a                -> usesIds a
+  EMApp n as              -> (C._nmIds n) ++ (um as) -- should the identifier parts of the name be in here?
+  EArrNew  _ as _         -> um as
+  EArrNewI _ _ as         -> um as
+  EInstNew n as           -> (C._nmIds n) ++ (um as)
+  ESysOut a               -> usesIds a
+  Block as                -> um as
+  SExpr a                 -> usesIds a
+  SVars d                 -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d) -- must think about list of maybe VarInits as well
+  SReturn a               -> usesIds a
+  SIf a1 a2               -> um [a1,a2]
+  SIfElse a1 a2 a3        -> um [a1,a2,a3]
+  SWhile a1 a2            -> um [a1,a2]
+  SDo a1 a2               -> um [a1,a2]
+  SForB ma1 ma2 mas a     -> um $ a : maybeToList ma1 ++
                                     maybeToList ma2 ++
                                     (concat . maybeToList) mas
-  (SForE _ id a1 a2)        -> id : um [a1,a2]
-  (SSwitch a as)            -> um $ a:as
-  (SwitchBlock l as)        -> undefined -- switchLabels contains expressions, which may include names, maybe just find the expression and convert to unitype?
-  (SwitchCase a)            -> usesIds a
-  (FIVars d)                -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d) -- must think about list of maybe VarInits as well
-  (FIExprs as)              -> um as
-  --(MethodDecl _ id fps as)  -> id : ((map (C._vdiIdent . C._fpVDI) fps) ++ um as)
+  SForE _ id a1 a2        -> id : um [a1,a2]
+  SSwitch a as            -> um $ a:as
+  SwitchBlock l as        -> undefined -- switchLabels contains expressions, which may include names, maybe just find the expression and convert to unitype?
+  SwitchCase a            -> usesIds a
+  FIVars d                -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d) -- must think about list of maybe VarInits as well
+  FIExprs as              -> um as
+  --(MethodDecl _ id fps as  -> id : ((map (C._vdiIdent . C._fpVDI) fps) ++ um as)
   --(CompilationUnit as1 as2) -> um $ as1 ++ as2
-  (ClassTypeDecl a)         -> usesIds a
-  --(ClassDecl id a)          -> id : usesIds a
-  (ClassBody as)            -> um as
-  (MemberDecl a)            -> usesIds a
-  _                         -> []
+  ClassTypeDecl a         -> usesIds a
+  --(ClassDecl id a          -> id : usesIds a
+  ClassBody as            -> um as
+  MemberDecl a            -> usesIds a
+  _                       -> []
   where um x = concat $ map usesIds x
 
 changesIds :: AST -> [C.Ident]
 changesIds = \case
-  (LVName n)                -> C._nmIds n -- find everything that could acctually reach this, should those be replaced by usesId?
-  (LVArray a as)            -> (usesIds a) ++ (cm $ as) -- should this be uses? also see above
-  (InitExpr a)              -> changesIds a
-  (InitArr  as)             -> cm as
-  (ECast _ a )              -> changesIds a
-  (ECond a1 a2 a3)          -> cm [a1,a2,a3]
-  (EAssign a1 a2)           -> cm [a1,a2] -- reaches LValue
-  (EOAssign a1 _ a2)        -> cm [a1,a2] -- reaches LValue
-  (ENum _ a1 a2)            -> cm [a1,a2]
-  (ECmp _ a1 a2)            -> cm [a1,a2]
-  (ELog _ a1 a2)            -> cm [a1,a2]
-  (ENot a)                  -> changesIds a
-  (EStep _ a)               -> changesIds a
-  (EBCompl a)               -> changesIds a
-  (EPlus a)                 -> changesIds a
-  (EMinus a)                -> changesIds a
-  (EMApp _ as)              -> cm as
-  (EArrNew  _ as _)         -> cm as
-  (EArrNewI _ _ as)         -> cm as
-  (ESysOut a)               -> changesIds a
-  (Block as)                -> cm as
-  (SExpr a)                 -> changesIds a
-  (SVars d)                 -> C._vdiIdent . C._vdVDI <$> C._tvdVDecls d
-  (SReturn a)               -> changesIds a
-  (SIf a1 a2)               -> cm [a1,a2]
-  (SIfElse a1 a2 a3)        -> cm [a1,a2,a3]
-  (SWhile a1 a2)            -> cm [a1,a2]
-  (SDo a1 a2)               -> cm [a1,a2]
-  (SForB ma1 ma2 mas a)     -> cm $ a : maybeToList ma1 ++
+  LVName n                -> C._nmIds n -- find everything that could acctually reach this, should those be replaced by usesId?
+  LVArray a as            -> (usesIds a) ++ (cm $ as) -- should this be uses? also see above
+  InitExpr a              -> changesIds a
+  InitArr  as             -> cm as
+  ECast _ a               -> changesIds a
+  ECond a1 a2 a3          -> cm [a1,a2,a3]
+  EAssign a1 a2           -> cm [a1,a2] -- reaches LValue
+  EOAssign a1 _ a2        -> cm [a1,a2] -- reaches LValue
+  ENum _ a1 a2            -> cm [a1,a2]
+  ECmp _ a1 a2            -> cm [a1,a2]
+  ELog _ a1 a2            -> cm [a1,a2]
+  ENot a                  -> changesIds a
+  EStep _ a               -> changesIds a
+  EBCompl a               -> changesIds a
+  EPlus a                 -> changesIds a
+  EMinus a                -> changesIds a
+  EMApp _ as              -> cm as
+  EArrNew  _ as _         -> cm as
+  EArrNewI _ _ as         -> cm as
+  EInstNew n as           -> (C._nmIds n) ++ (cm as)
+  ESysOut a               -> changesIds a
+  Block as                -> cm as
+  SExpr a                 -> changesIds a
+  SVars d                 -> C._vdiIdent . C._vdVDI <$> C._tvdVDecls d
+  SReturn a               -> changesIds a
+  SIf a1 a2               -> cm [a1,a2]
+  SIfElse a1 a2 a3        -> cm [a1,a2,a3]
+  SWhile a1 a2            -> cm [a1,a2]
+  SDo a1 a2               -> cm [a1,a2]
+  SForB ma1 ma2 mas a     -> cm $ a : maybeToList ma1 ++
                                     maybeToList ma2 ++
                                     (concat . maybeToList) mas
-  (SForE _ id a1 a2)        -> id : cm [a1,a2]
-  (SSwitch a as)            -> cm $ a:as
-  (SwitchBlock l as)        -> undefined -- switchLabels contains expressions, which may include names, maybe just find the expression and convert to unitype?
-  (SwitchCase a)            -> changesIds a
-  (FIVars d)                -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d)
-  (FIExprs as)              -> cm as
-  --(MethodDecl _ id fps as)  -> id : ((map (C._vdiIdent . C._fpVDI) fps) ++ cm as)
+  SForE _ id a1 a2        -> id : cm [a1,a2]
+  SSwitch a as            -> cm $ a:as
+  SwitchBlock l as        -> undefined -- switchLabels contains expressions, which may include names, maybe just find the expression and convert to unitype?
+  SwitchCase a            -> changesIds a
+  FIVars d                -> map (C._vdiIdent . C._vdVDI) (C._tvdVDecls d)
+  FIExprs as              -> cm as
+  --(MethodDecl _ id fps as  -> id : ((map (C._vdiIdent . C._fpVDI) fps) ++ cm as)
   --(CompilationUnit as1 as2) -> cm $ as1 ++ as2
-  (ClassTypeDecl a)         -> changesIds a
-  --(ClassDecl id a)          -> id : changesIds a
-  (ClassBody as)            -> cm as
-  (MemberDecl a)            -> changesIds a
-  _                         -> []
+  ClassTypeDecl a         -> changesIds a
+  --(ClassDecl id a          -> id : changesIds a
+  ClassBody as            -> cm as
+  MemberDecl a            -> changesIds a
+  _                       -> []
   where cm x = concat $ map changesIds x
 
 nbrOfStatements :: AST -> Int
