2b069b6337ddac8f99635f84b3d1a563b5621ffc
alekaar <sternkaar@gmail.com>
2017-04-04 20:15:02 +0200

    small test for float to double

---
+++ Test/fixture/norm/floattodouble/Expected1.java
@@ -0,0 +1,9 @@
+public class C{
+  public static void main(String[] args) {
+      double x;
+  }
+
+  public static double lol(){}
+
+
+}
---
+++ Test/fixture/norm/floattodouble/Source1.java
@@ -0,0 +1,9 @@
+public class C{
+  public static void main(String[] args) {
+      float x;
+  }
+
+  public static float lol(){}
+
+
+}
