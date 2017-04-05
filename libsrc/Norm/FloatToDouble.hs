0f172113cf826c2022616f5924a3f9dde71f08da
alekaar <sternkaar@gmail.com>
2017-04-05 15:25:02 +0200

    added rules to allnormalizations

--- libsrc/Norm/FloatToDouble.hs
+++ libsrc/Norm/FloatToDouble.hs
@@ -25,18 +25,18 @@ import Norm.NormCS
 
 -- TODO allocate stages. At the moment chosen arbitrarily.
 stage :: Int
-stage = 16
+stage = 15
 
--- | Rule to transform float into double
+-- | Rule to transform float vars into double vars
 -- > float x; => double x;
 normFloatToDoubleVars :: NormCUR
 normFloatToDoubleVars = makeRule' "unsafe.coerce_float_to_double.vardecl"
                                   [stage] execFloatToDoubleVars
 
--- > float x; => double x;
+-- > public static float x(){} => public static double x(){}
 normFloatToDoubleRet :: NormCUR
 normFloatToDoubleRet = makeRule' "unsafe.coerce_float_to_double.method"
-                                 [stage+1] execFloatToDoubleRet
+                                 [stage + 1] execFloatToDoubleRet
 
 -- | Transforms float variable inits, decls and references in to doubles
 execFloatToDoubleVars :: NormCUA
