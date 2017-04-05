937662eb676d85401eae4b1f9892fe8b23f512ec
alekaar <sternkaar@gmail.com>
2017-04-05 15:18:52 +0200

    tested, working for both variables and methods

--- Test/fixture/norm/floattodouble/Source1.java
+++ Test/fixture/norm/floattodouble/Source1.java
@@ -1,9 +1,55 @@
 public class C{
   public static void main(String[] args) {
-      float x;
+      float xa;
+      float f, g;
+      float l = 1, u = 2;
+      if(true){
+        float xb;
+      }
+      else{
+        float xc;
+      }
+
+      if(true){
+        float xd;
+      }
+      for(;l == 1;){
+        float xe;
+      }
+      while(u == 2){
+        float xf;
+      }
+
+      {
+        float xg;
+      }
+
   }
 
-  public static float lol(){}
+  public static float lol(float y){
+    float xa;
+    float f, g;
+    float l = 1, u = 2;
+    if(true){
+      float xb;
+    }
+    else{
+      float xc;
+    }
 
+    if(true){
+      float xd;
+    }
+    for(;l == 1;){
+      float xe;
+    }
+    while(u == 2){
+      float xf;
+    }
 
+    {
+      float xg;
+    }
+    return u;
+  }
 }
