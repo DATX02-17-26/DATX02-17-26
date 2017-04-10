5f2a7f98d782d5fa9d51ca75a3986442e2747240
Mazdak Farrokhzad <twingoow@gmail.com>
2017-04-10 08:01:22 +0200

    Util.Function: added various helpers.

--- libsrc/Util/Function.hs
+++ libsrc/Util/Function.hs
@@ -16,24 +16,6 @@
  - Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  -}
 
-{- Javalette Compiler, a simple C like language.
- - Copyright, 2016, Mazdak Farrokhzad
- -
- - This program is free software; you can redistribute it and/or
- - modify it under the terms of the GNU General Public License
- - as published by the Free Software Foundation; either version 2
- - of the License, or (at your option) any later version.
- -
- - This program is distributed in the hope that it will be useful,
- - but WITHOUT ANY WARRANTY; without even the implied warranty of
- - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- - GNU General Public License for more details.
- -
- - You should have received a copy of the GNU General Public License
- - along with this program; if not, write to the Free Software
- - Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- -}
-
 {-|
 Module      : Util.Function
 Description : General utility for functions and arrows.
@@ -47,7 +29,12 @@ General utility for functions and arrows.
 module Util.Function (
     -- * Operations
     untilMatch, untilEq, applyN
+  , (-|), (|-), (|=), (=|)
 ) where
+
+--------------------------------------------------------------------------------
+-- credit: previous work in "Compiler Construction" by Mazdak, 2016.
+--------------------------------------------------------------------------------
 
 -- | 'untilMatch': applies the second argument which is the function f, starting
 -- with the value in the third argument, which is x, and replaces it with that
@@ -68,3 +55,26 @@ untilEq = untilMatch (==)
 applyN :: (Eq i, Num i) => i -> (a -> a) -> a -> a
 applyN 0 _ x = x
 applyN i f x = applyN (i - 1) f $ f x
+
+--------------------------------------------------------------------------------
+-- credit: http://stackoverflow.com/a/6358630/1063961
+--------------------------------------------------------------------------------
+
+-- | Left associative flipped function application.
+(-|) :: a -> (a -> b) -> b
+(-|) = flip ($)
+
+-- | Left associative function application.
+(|-) :: (a -> b) -> a -> b
+(|-) = ($)
+
+-- | Right associative flipped function application of binary function.
+(|=) :: (a -> b -> c) -> b -> a -> c
+(|=) = flip
+
+-- | Right associative function application.
+(=|) :: a -> (a -> c) -> c
+(=|) = (-|)
+
+infixl 3 -|, |-
+infixr 3 =|, |=
