6d7fb4a5bbf4fc63249122f4d21c2eee5842cad3
Maximilian Algehed <m.algehed@gmail.com>
2017-04-11 11:11:55 +0200

    Changing model solutions

--- modelsolution/uppgift12a/Uppgift12a_4.java
+++ modelsolution/uppgift12a/Uppgift12a_4.java
@@ -1,17 +1,17 @@
 public class Uppgift12a_4 {
    public static void main(String[] args) {
 
-      double pi = 0;
+      double pi = 1;
       double denominator = 1;
 
       for (int i = 1; i <= 500; i = i + 1){
 
+         denominator = denominator + 2;
          if (i % 2 == 0) {
             pi = pi + (1 / denominator);
          } else {
             pi = pi - (1 / denominator);
          }
-         denominator = denominator + 2;
       }
       pi = pi * 4;
       System.out.println(pi);
